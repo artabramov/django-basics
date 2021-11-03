b22871f04a83f1c56c46aff60b95219756d966ab
vagrant <vagrant@vagrant.vm>
2021-11-03 17:51:23 +0000

    done hw-8

--- admins/views.py
+++ admins/views.py
@@ -1,12 +1,60 @@
 from django.http import response
 from django.shortcuts import render, HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import user_passes_test
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from users.models import User
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 
 # Create your views here.
+
+class UserListView(ListView):
+    model = User
+    template_name = 'admins/admin-users-read.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(UserListView, self).get_context_data(**kwargs)
+        context['title'] = 'Users list'
+        return context
+
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserListView, self).dispatch(request, *args, **kwargs)
+
+
+class UserCreateView(CreateView):
+    model = User
+    template_name = 'admins/admin-users-create.html'
+    form_class = UserAdminRegistrationForm
+    success_url = reverse_lazy('admins:admin_users_read')
+
+
+class UserUpdateView(UpdateView):
+    model = User
+    template_name = 'admins/admin-users-update-delete.html'
+    form_class = UserAdminProfileForm
+    success_url = reverse_lazy('admins:admin_users_read')
+
+    def get_context_data(self, **kwargs):
+        context = super(UserUpdateView, self).get_context_data(**kwargs)
+        context['title'] = 'User update'
+        return context
+
+
+class UserDeleteView(DeleteView):
+    model = User
+    template_name = 'admins/admin-users-update-delete.html'
+    success_url = reverse_lazy('admins:admin_users_read')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        success_url = self.get_success_url()
+        self.object.ban()
+        return HttpResponseRedirect(success_url)
+
 
 @user_passes_test(lambda u: u.is_staff)
 def index(request):
@@ -15,7 +63,7 @@ def index(request):
     }
     return render(request, 'admins/index.html', context)
 
-
+"""
 @user_passes_test(lambda u: u.is_staff)
 def admin_users_create(request):
 
@@ -78,4 +126,4 @@ def admin_users_delete(request, user_id):
 
     selected_user.ban()
     return HttpResponseRedirect(reverse('admins:admin_users_read'))
-
+"""
